ba3f497a280946739cbc87b3a87549423e9e8e7b
Melinati Deepika <melinatideepika@gmail.com>
2025-09-02 11:38:08 +0530

    Update add.hs

--- add.hs
+++ add.hs
@@ -1,3 +1,12 @@
+-- Define the function
+add :: Num a => a -> a -> a
 add a b = a + b
 
--- add 10 20 is same as 10 `add` 20
+-- Main function to run
+main :: IO ()
+main = do
+    let x = add 10 30           -- normal prefix style
+    let y = 10 `add` 30         -- infix style using backticks
+    putStrLn ("add 10 30 = " ++ show x)
+    putStrLn ("10 `add` 30 = " ++ show y)
+
